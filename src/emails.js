c23d414b6d2f5c27d232446724d3480250bf96f1
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-04-19 19:19:45 +0200

    Add email content for accepted participation & new participant notification

--- src/emails.js
+++ src/emails.js
@@ -56,11 +56,72 @@ const sendEmailChangeConfirmation = () => {};
 
 const sendNewOrg = (to, displayName) => {};
 
-const sendNewParticipant = (to, displayName, participant) => {};
+/**
+ * Sends email to issuer when participant subscribes to learning opportunity
+ * @param {{ to, opportunity: { title, id }, participant: { name, email } }} data Input data for the email
+ * @param {string} data.to email address of the issuer
+ * @param {string} data.opportunity.title title of the opportunity
+ * @param {string} data.opportunity.id id of the opportunity
+ * @param {string} data.participant.name name of the participant
+ * @param {string} data.participant.email email address of the participant
+ */
+const sendNewParticipant = ({ to, opportunity: { title, id }, participant: { name, email } }) => {
+  const content = `
+    <p>Dag partner van Gentlestudent,</p>
+    
+    <p>Er heeft zich zopas iemand ingeschreven voor de leerkans: "${title}"</p>
+  
+    <p>De gegevens van deze persoon zijn: <br />
+    - Naam: ${name}<br />
+    - E-mailadres: ${email}</p>
+
+    <p>Op <a href="https://gentlestudent.gent/opportunities/${id}">deze pagina</a> kan je jouw leerkans terugvinden,
+    en indien je bent ingelogd kan je er de deelnemer accepteren. Zodra je de deelnemer accepteert,
+    zal hij/zij een bevestigingsmail krijgen. Pas daarna kan de deelnemer met jou contact opnemen om verder af te stemmen.</p>
+    
+    <p>Met vriendelijke groet,</p>
+    
+    <p>Team Gentlestudent</p>`;
+
+  sendMail({
+    to,
+    subject: `Inschrijving voor leerkans: ${title}`,
+    text: '',
+    html: content
+  });
+};
 
 const sendBadgeClaimed = (to, displayName, participant) => {};
 
-const sendAcceptedParticipation = (to, displayName, org) => {};
+/**
+ * Sends email to participant when issuer has approved them
+ * @param {{ opportunity: { title }, participant: { participantName, participantEmail }, issuer: { issuerName, issuerEmail }}} data Input data for the email
+ * @param {string} opportunity.title title of the opportunity
+ * @param {string} participant.participantName name of the participant
+ * @param {string} participant.participantEmail email of the participant (the email recipient)
+ * @param {string} issuer.issuerEmail email address of the issuer
+ * @param {string} issuer.issuerName name of the issuing organisation
+ */
+const sendAcceptedParticipation = ({
+  opportunity: { title },
+  participant: { participantName, participantEmail },
+  issuer: { issuerName, issuerEmail }
+}) => {
+  const content = `
+    <p>Dag ${participantName}</p>
+   
+    ${issuerName} heeft je registratie voor de leerkans "${title}" geaccepteerd. Je kan nu contact opnemen met de organisatie via mail.
+    <p> - E-mailadres: ${issuerEmail} </p>
+    
+    <p>Veel succes!</p>
+    <p>Team Gentlestudent</p>`;
+
+  sendMail({
+    to: participantEmail,
+    subject: `Geaccepteerd voor leerkans: ${title}`,
+    html: content
+  });
+};
 
 const sendRejectedParticipation = (to, displayName, org) => {};
 
